8d0b8774e9a2c132b6d073441b93844d652897b5
frank <18652171101@163.com>
2017-03-27 15:22:07 +0800

    电子书

--- src/pages/dzs/BookList.vue
+++ src/pages/dzs/BookList.vue
@@ -4,22 +4,19 @@
         <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
             <el-form :inline="true" :model="filters">
                 <el-form-item>
-                    <el-input v-model="filters.keyword" placeholder="名称或标签"></el-input>
+                    <el-input v-model="filters.keyword" placeholder="名称或作者"></el-input>
                 </el-form-item>
                 <el-select v-model="category" placeholder="请选择" v-on:change="getBooks">
-                    <el-option v-for="item in catergories" :key="item.value" :label="item.label" :value="item.value">
+                    <el-option v-for="item in categories" :key="item.id" :label="item.name" :value="item.name">
                     </el-option>
                 </el-select>
                 <el-form-item>
-                    <el-button type="primary" v-on:click="getAlbums">查询</el-button>
+                    <el-button type="primary" v-on:click="getBooks">查询</el-button>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" @click="handleAddToApp" :disabled="this.sels.length===0">添加至APP
-
+                    <el-button type="primary" @click="handleAddToApp" :disabled="this.sels.length===0">
+                        添加至APP
                     </el-button>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" @click="handleAdd">添加图书</el-button>
                 </el-form-item>
             </el-form>
         </el-col>
@@ -30,8 +27,7 @@
             <el-table-column prop="id" label="ID" width="100"></el-table-column>
             <el-table-column prop="title" label="名称" min-width="150"></el-table-column>
             <el-table-column prop="author" label="作者" min-width="150"></el-table-column>
-            <el-table-column prop="DzsCategory.title" label="类型" width="120"></el-table-column>
-            <el-table-column label="标签" min-width="150">
+            <el-table-column label="分类" min-width="150">
                 <template scope="scope">
                     <el-tag v-for="tag in scope.row.tags" :key="tag" type="gray">{{tag}}</el-tag>
                 </template>
@@ -51,21 +47,6 @@
                            :total="total" style="float:right;">
             </el-pagination>
         </el-col>
-
-        <el-dialog title="添加" v-model="addFormVisible" :close-on-click-modal="false">
-            <el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
-                <el-form-item label="名称" prop="title">
-                    <el-input v-model="addForm.title" auto-complete="off"></el-input>
-                </el-form-item>
-                <el-form-item label="作者" prop="title">
-                    <el-input v-model="addForm.author" auto-complete="off"></el-input>
-                </el-form-item>
-            </el-form>
-            <div slot="footer" class="dialog-footer">
-                <el-button @click.native="addFormVisible = false">取消</el-button>
-                <el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
-            </div>
-        </el-dialog>
 
         <!--编辑界面-->
         <el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false">
@@ -89,7 +70,7 @@
             </el-form>
             <div slot="footer" class="dialog-footer">
                 <el-button @click.native="addToAppFormVisible = false">取消</el-button>
-                <el-button type="primary" @click.native="addToApp" :loading="addLoading">提交</el-button>
+                <el-button type="primary" @click.native="addToApp" :loading="addToAppLoading">提交</el-button>
             </div>
         </el-dialog>
     </section>
@@ -103,8 +84,8 @@
   export default {
     data() {
       return {
-        catergories: [],
-        category: '',
+        categories: [],
+        category: '所有分类',
         filters: {
           name: ''
         },
@@ -126,19 +107,6 @@
         editForm: {
           id: 0,
         },
-        addFormVisible: false, //新增界面是否显示
-        addLoading: false,
-        addForm: {
-          appId: ''
-        },
-        addFormRules: {
-          appId: [{
-            required: true,
-            message: '请输入AppId',
-            trigger: 'blur'
-          }]
-        },
-
         addToAppFormVisible: false, //新增界面是否显示
         addToAppLoading: false,
         addToAppForm: {
@@ -153,6 +121,11 @@
         }
       }
     },
+    filters: {
+      imgPath(val){
+        return `http://book.hizuoye.com/images/full/${val}`
+      }
+    },
     methods: {
 
       handleCurrentChange(val) {
@@ -160,31 +133,31 @@
         this.getBooks()
       },
 
+      getCategories(){
+        dzs.getCategories().then((res) => {
+          let categories = res.data
+          categories.unshift({id: 999, name: '所有分类'})
+          this.categories = categories
+        })
+      },
       getBooks() {
         let para = {
           page: this.page,
-          search: this.filters.keyword,
-          DzsCategoryId: this.category
+          keyword: this.filters.keyword,
+          tag: this.category === "所有分类" ? undefined : this.category
         }
         this.listLoading = true
         dzs.getBooks(para).then((res) => {
           this.total = res.data.count
-          this.albums = res.data.rows
+          this.books = res.data.rows
           this.listLoading = false
         })
       },
-
       //显示编辑界面
       handleEdit: function (index, row) {
         this.editFormVisible = true
         this.editForm = Object.assign({}, row)
       },
-
-      handleAdd: function () {
-        this.addFormVisible = true
-        this.addToAppForm = {title: '', author: ''}
-      },
-
       handleAddToApp: function () {
         this.addToAppFormVisible = true
         this.addToAppForm = {appId: ''}
@@ -215,43 +188,39 @@
           }
         })
       },
-
       selsChange: function (sels) {
         this.sels = sels
       },
-
-      formatDate: function (row) {
-        return row.createdAt.substr(0, 10)
-      },
-
       addToApp: function () {
-        this.$refs.addForm.validate((valid) => {
+        this.$refs.addToAppForm.validate((valid) => {
           if (valid) {
-            this.editLoading = true
-            let appId = this.addForm.appId
+            this.addToAppLoading = true
+            let appId = this.addToAppForm.appId
             let bookIds = this.sels.map(item => item.id)
-//            addAlbumsToApp({
-//              appId,
-//              albumIds
-//            }).then((res) => {
-//              this.addLoading = false
-//              this.$message({
-//                message: '添加成功',
-//                type: 'success'
-//              })
-//              this.$refs['addForm'].resetFields()
-//              this.addFormVisible = false
-//            }).catch((err) => {
-//              this.addLoading = false
-//              let {message} = err.response.data
-//              this.$message.error(message || err.message)
-//            })
+            dzs.addToApp({
+              appId,
+              bookIds
+            }).then((res) => {
+              this.addToAppLoading = false
+              this.$message({
+                message: '添加成功',
+                type: 'success'
+              })
+              this.$refs['addToAppForm'].resetFields()
+              this.addToAppFormVisible = false
+            }).catch((err) => {
+              console.log(err)
+              this.addToAppLoading = false
+              let {message} = err.response.data
+              this.$message.error(message || err.message)
+            })
           }
         })
       }
     },
     mounted() {
       this.getBooks()
+      this.getCategories()
     }
   }
 </script>
